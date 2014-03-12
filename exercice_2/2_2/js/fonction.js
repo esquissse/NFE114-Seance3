7f2c4d0680966480c9e16ccebb04a93214f578a7
michael <mwinckler@chu-reims.fr>
2014-03-12 10:22:33 +0100

    exercice 2.2 opérationnel

--- exercice_2/2_2/js/fonction.js
+++ exercice_2/2_2/js/fonction.js
@@ -1,7 +1,32 @@
 document.getElementById('changer').addEventListener('click', changer_devise, false);
-document.getElementById('franc_euro').addEventListener('click', convertir_franc_euro, false);
-document.getElementById('euro_franc').addEventListener('click', convertir_euro_franc, false);
+document.getElementById('convertisseur').addEventListener('click', convertir, false);
 
 function changer_devise() {
-  document.getElementById('sens_du_change').innerHTML = "<input type='button' id='euro_franc' value='F -> E'/>";
+  var into_span = document.getElementById('sens_du_change');
+  
+  // Comme je modifie le DOM en cours de route, il faut repositionner le listener :
+  if ( document.getElementById('convertisseur').value == "E -> F" ) {
+    into_span.innerHTML = "<input type='button' id='convertisseur' value='F -> E'/>";
+    document.getElementById('convertisseur').addEventListener('click', convertir, false);
+  } else {
+    into_span.innerHTML = "<input type='button' id='convertisseur' value='E -> F'/>";
+    document.getElementById('convertisseur').addEventListener('click', convertir, false);
+  }
+}
+
+function convertir() {
+  var valeur_a_convertir = document.getElementById('valeur_de_change').value;
+  if ( document.getElementById('convertisseur').value == "E -> F" ) {
+    document.getElementById('resultat').innerHTML = arrondir(valeur_a_convertir * 6.55957) + ' F';
+  } else {
+    document.getElementById('resultat').innerHTML = arrondir(valeur_a_convertir / 6.55957) + ' €';
+  }
+}
+
+function arrondir(chiffre) {
+  if (chiffre.toFixed) {
+    return chiffre.toFixed(2);
+  } else {
+    return Math.round(chiffre*100)/100;
+  }
 }
